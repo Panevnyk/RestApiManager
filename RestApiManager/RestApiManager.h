4893acd5ad60b715422ce40d5a4b82395034b33c
panevnyk <sieter3008@gmail.com>
2018-02-28 09:19:44 +0200

    Initial Commit

---
+++ RestApiManager/RestApiManager.h
@@ -0,0 +1,19 @@
+//
+//  RestApiManager.h
+//  RestApiManager
+//
+//  Created by Panevnyk Vlad on 2/28/18.
+//  Copyright © 2018 Panevnyk Vlad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RestApiManager.
+FOUNDATION_EXPORT double RestApiManagerVersionNumber;
+
+//! Project version string for RestApiManager.
+FOUNDATION_EXPORT const unsigned char RestApiManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RestApiManager/PublicHeader.h>
+
+
