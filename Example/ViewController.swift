f80180f336491f580d72aebee5643404db3b98f3
Panevnyk <sieter3008@gmail.com>
2018-11-15 14:08:36 +0200

    add Example target to RestApiManager

---
+++ Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Panevnyk Vlad on 11/15/18.
+//  Copyright © 2018 Panevnyk Vlad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
